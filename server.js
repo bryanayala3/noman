182d149d5c2a32cbb2e906ddaabb69b4de8151d9
bryanayala3 <git config --global user.name Bryan>
2023-03-09 15:11:31 -0500

    news

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var express = require("express");
 var app = express();
 
 app.get("/", (req, res) => {
-  res.send("<strong>Home</strong>");
+  res.send("<strong>Bryan Ayala</strong>");
 });
 
 app.listen(HTTP_PORT);
