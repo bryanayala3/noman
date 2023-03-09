21a91d862b3f90a44213598e2228202a8b82d1a1
bryanayala3 <git config --global user.name Bryan>
2023-03-09 14:34:02 -0500

    server

---
+++ server.js
@@ -0,0 +1,9 @@
+var HTTP_PORT = process.env.port || 8080;
+var express = require("express");
+var app = express();
+
+app.get("/", (req, res) => {
+  res.send("Hello form Bryan Ayala");
+});
+
+app.listen(HTTP_PORT);
